c9538af53ea49ed791e4420aa177db50411c7315
Daphne Kramer <99046027@mydavinci.nl>
2020-02-14 12:17:35 +0100

    Blok 3
    Week 2

---
+++ onkunde.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <h1>Onkunde</h1>
+        <p>Er zijn veel mensen die niet kunnen <?php echo $_POST["kunnen"];?>. Neem nou <?php echo $_POST["persoon"];?>. Zelfs met de hulp van een <?php echo $_POST["vakantie"];?> of zelfs <?php echo $_POST["getal"];?> kan <?php echo $_POST["persoon"];?> niet <?php echo $_POST["kunnen"];?>. Dat heeft niet te maken met een gebrek aan <?php echo $_POST["beste"];?>, maar met een te veel aan <?php echo $_POST["slechtste"];?>. Te veel <?php echo $_POST["slechtste"];?> leidt tot <?php echo $_POST["ergste"];?> en dat is niet goed als je wilt <?php echo $_POST["kunnen"];?>. Helaas voor <?php echo $_POST["persoon"];?></p>
+
+    </body>
+</html>
---
+++ paniek.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <h1>Er heerst paniek...</h1>
+        <p>Er heeft paniek in het koninkrijk <?php echo $_POST["land"];?>, Koning <?php echo $_POST["docent"];?> is ten einde raad en als koning <?php echo $_POST["docent"];?> ten einde raad is, dan roopt hij zijn ten-einde-raadsheer <?php echo $_POST["persoon"];?> </p>
+
+        <p>"<?php echo $_POST["persoon"];?>! Het is een ramp! Het is een schande!</p>
+        <p>"Sire, Majesteit, Uwe Luidruchtigheid, wat is er aan de hand?"</p>
+        <p>"Mijn <?php echo $_POST["huisdier"];?> is verdwenen! Zo maar, zonder waarschuwing. En ik had net <?php echo $_POST["speelgoed"];?> voor hem gekocht!"</p>
+        <p>"Majesteit, uw <?php echo $_POST["huisdier"];?> komt vast vanzelf weer terug?"</p>
+        <p>"Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd leren <?php echo $_POST["bezigheid"];?>?"</p>
+        <p>"Maar Sire, daar kunt u toch uw <?php echo $_POST["euro"];?> voor gebruiken?"</p>
+        <p>"<?php echo $_POST["persoon"];?>, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had."</p>
+        <p>"<?php echo $_POST["verveelt"];?>, Sire"</p>
+    </body>
+</html>
